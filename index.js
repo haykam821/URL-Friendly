0f84e9797fc690acf55e0a1f07187046ce482639
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-08-01 12:24:27 -0400

    Fix ESLint errors

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /**
  * Makes friendly text for use in URLs.
  * @param {string} text The text to make friendly for URLs.
- * @param {Object} [opts] Other options.
- * @param {boolean} [opts.collapse=true] If true, multiple unfriendly characters in a row only produce one dash.
- * @param {RegExp} [opts.unsafeCharRegex] The expression to match unsafe characters.
+ * @param {Object} [options] Other options.
+ * @param {boolean} [options.collapse=true] If true, multiple unfriendly characters in a row only produce one dash.
+ * @param {RegExp} [options.unsafeCharRegex] The expression to match unsafe characters.
  * @returns {string}
  */
-function urlFriendly(text, opts) {
+function urlFriendly(text, options) {
 	if (typeof text !== "string") {
 		const error = new TypeError("The text to slugify must be a string.");
 		error.code = "TEXT_NOT_STRING";
 		throw error;
 	}
 
-	const realOpts = {
+	const realOptions = {
 		collapse: true,
 		unsafeCharRegex: /[^a-zA-Z0-9_-]/g,
-		...opts,
+		...options,
 	};
 
-	const slugged = text.trim().replace(realOpts.unsafeCharRegex, "-");
-	return realOpts.collapse ? slugged.replace(/-+/g, "-") : slugged;
+	const slugged = text.trim().replace(realOptions.unsafeCharRegex, "-");
+	return realOptions.collapse ? slugged.replace(/-+/g, "-") : slugged;
 }
 module.exports = urlFriendly;
